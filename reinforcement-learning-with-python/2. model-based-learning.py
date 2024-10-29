2414cfef94be9a0ddf705b87ab308335654e24ed
Vishnu B <v.bindiganavile@gmail.com>
2024-10-29 12:05:06 -0400

    finished chapter 2 and 3 for reinforcement learning with python

--- reinforcement-learning-with-python/2. model-based-learning.py
+++ reinforcement-learning-with-python/2. model-based-learning.py
@@ -1,1 +1,7 @@
 import gymnasium as gym
+
+env = gym.make('FrozenLake', is_slippery=True) # turn to stochastic
+print(env.action_space) # 4
+print(env.observation_space) # 16
+print(env.unwrapped.P)
+# env.unwrapped.P[state][action] --> (probability, s', r, terminal?)
